eff23dcdb1b89d435f60353fa186c737daa52eff
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-05 12:40:06 +0200

    feature: support taking sum over only some axes

--- crater/operations/sum.py
+++ crater/operations/sum.py
@@ -1,13 +1,21 @@
 import numpy as np
 from crater.tensor import Tensor
 from crater.gradient import Gradients, Gradient
+from crater.utils import tuplify
 
 
-def sum(tensor: Tensor):
+def sum(tensor: Tensor, axes=None):
+    axes = tuplify(range(len(tensor.shape)) if axes is None else axes)
     return Tensor.from_numpy(
-        data=tensor.data.sum(),
+        data=tensor.data.sum(axes),
         backward=lambda gradient: Gradients.accumulate(
-            Gradient(tensor=tensor, gradient=np.full_like(tensor.data, gradient))
+            Gradient(
+                tensor=tensor,
+                gradient=np.tile(
+                    np.expand_dims(gradient, axes),
+                    [dim if idx in axes else 1 for idx, dim in enumerate(tensor.shape)],
+                ),
+            )
         ),
     )
 
--- test/operations/test_sum.py
+++ test/operations/test_sum.py
@@ -2,12 +2,24 @@ import numpy as np
 from crater import Tensor
 
 
-def test_sum():
+def test_full():
     assert Tensor.from_builtin([1, 2, -1]).sum() == Tensor.from_builtin(2)
 
 
-def test_gradients():
+def test_specific():
+    assert Tensor.from_builtin([[1, 4], [5, 0]]).sum(0) == Tensor.from_builtin([6, 4])
+    assert Tensor.from_builtin([[1, 4], [5, 0]]).sum(1) == Tensor.from_builtin([5, 5])
+
+
+def test_gradients_full():
     tensor = Tensor.from_builtin([2, 3, 4])
     result = tensor.sum()
     gradients = result.backward(np.array(-1))
     assert np.all(gradients[tensor] == [-1, -1, -1])
+
+
+def test_gradients_specific():
+    tensor = Tensor.from_builtin([[5, 2], [9, 1]])
+    result = tensor.sum(1)
+    gradients = result.backward(np.array([1, 3]))
+    assert np.all(gradients[tensor] == [[1, 1], [3, 3]])
