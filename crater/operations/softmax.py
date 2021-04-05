670ebce959a8f0ccab7c77b0fa5a7370cb3e92dd
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-05 12:57:36 +0200

    feature: softmax over specific axes

--- crater/operations/softmax.py
+++ crater/operations/softmax.py
@@ -1,3 +1,9 @@
-def softmax(tensor):
+from crater.tensor import Tensor
+
+
+def softmax(tensor: Tensor, axes=None):
     exp_tensor = tensor.exp()
-    return exp_tensor / exp_tensor.sum()
+    return exp_tensor / exp_tensor.sum(axes=axes).expand_dims(axes=axes)
+
+
+Tensor.softmax = softmax
--- test/operations/test_softmax.py
+++ test/operations/test_softmax.py
@@ -1,18 +1,37 @@
 import numpy as np
 from crater import Tensor, Gradients, Gradient
-from crater.operations import softmax
 
 
-def test_zero_one():
-    assert softmax(Tensor.from_builtin([0, 1])) == Tensor.from_builtin(
+def test_full():
+    assert Tensor.from_builtin([0, 1]).softmax() == Tensor.from_builtin(
         [1 / (1 + np.e), np.e / (1 + np.e)]
     )
 
 
-def test_gradients():
+def test_one_axis():
+    target = [1 / (1 + np.e), np.e / (1 + np.e)]
+    assert Tensor.from_builtin([[0, 1], [1, 0]]).softmax(1) == Tensor.from_builtin(
+        [target, target[::-1]]
+    )
+
+
+def test_gradients_full():
     logits = Tensor.from_builtin([1, 2, 3])
-    probabilities = softmax(logits)
+    probabilities = logits.softmax()
     gradients = Gradients._trace(
         Gradient(tensor=probabilities, gradient=np.array([1, 0, 0]))
     )
     assert np.allclose(gradients[logits], [0.0819, -0.0220, -0.0599], atol=1e-4)
+
+
+def test_gradients_one_axis():
+    logits = Tensor.from_builtin([[1, 2, 3], [4, 5, 6]])
+    probabilities = logits.softmax(1)
+    gradients = Gradients._trace(
+        Gradient(tensor=probabilities, gradient=np.array([[1, 0, 0], [0, 1, 0]]))
+    )
+    assert np.allclose(
+        gradients[logits],
+        [[0.0819, -0.0220, -0.0599], [-0.0220, 0.1848, -0.1628]],
+        atol=1e-4,
+    )
