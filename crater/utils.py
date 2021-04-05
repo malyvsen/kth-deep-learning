85bf785d3310e0769590c33e7143b4f9bf5a322f
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-05 16:54:45 +0200

    chore: assignment 1

--- crater/utils.py
+++ crater/utils.py
@@ -1,5 +1,15 @@
+import numpy as np
+from .tensor import Tensor
+
+
 def tuplify(thing):
     try:
         return tuple(thing)
     except TypeError:
         return (thing,)
+
+
+def one_hot(labels, num_classes):
+    result = np.zeros([len(labels), num_classes])
+    result[np.arange(len(labels)), labels] = 1
+    return Tensor.from_numpy(result)
