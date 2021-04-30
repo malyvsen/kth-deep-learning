94d11e745df02b04d18a3e932a2c01e9c0eadb03
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-30 22:52:27 +0200

    chore: bump version

--- crater/__init__.py
+++ crater/__init__.py
@@ -1,4 +1,7 @@
+from importlib.metadata import distribution
 from crater.tensor import Tensor
 from crater.gradient import Gradients, Gradient
 import crater.operations
 from .layer import Layer
+
+__version__ = distribution("crater").version
